bd5d3d7519fd8b9412933be821bd7d5a9d991044
streamich <team@streamich.com>
2015-06-22 23:43:41 +0100

    First commit

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 
 
 function portable_transform_wrap(file, props) {
+
+    // Change file extension.
     if(props.ext) {
-        // TODO: change extension
+        var path = require('path');
+        var ext = path.extname(file.filepath);
+        file.filepath = file.filepath.substr(0, file.filepath.length - ext.length) + '.' + props.ext;
     }
+
+    // Wrap file contents into a `.js` function.
     file.raw = props.func + '(' + JSON.stringify(file.raw) + ')'
 }
 
-module.exports = file;
+module.exports = portable_transform_wrap;
